2226b05e0fbccbdee0f6c5ddcd88545c7545ef67
mondgnu <mondgnu@gmail.com>
2023-06-24 16:59:32 +0200

    Added PlayerProvider class

---
+++ src/main/java/de/yggdrasil/core/util/EnhancedPlayerProvider.java
@@ -0,0 +1,17 @@
+package de.yggdrasil.core.util;
+
+import net.minestom.server.entity.Player;
+import net.minestom.server.network.PlayerProvider;
+import net.minestom.server.network.player.PlayerConnection;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class EnhancedPlayerProvider implements PlayerProvider {
+    @Override
+    public @NotNull Player createPlayer(@NotNull UUID uuid, @NotNull String username, @NotNull PlayerConnection connection) {
+        // Load Player Information
+
+        return null;
+    }
+}
