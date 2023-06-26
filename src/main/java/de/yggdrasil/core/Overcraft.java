5200cf385c7435201b03b3eb7ba202f851ef509d
mondgnu <mondgnu@gmail.com>
2023-06-26 17:28:57 +0200

    Added the Overcraft class for central data access

---
+++ src/main/java/de/yggdrasil/core/Overcraft.java
@@ -0,0 +1,17 @@
+package de.yggdrasil.core;
+
+/**
+ * This class provides a centralized point were you can put classes which are needed all over the project
+ */
+public class Overcraft {
+    private static Overcraft overcraft = new Overcraft();
+
+    private Overcraft() {
+
+    }
+
+    public static Overcraft get() {
+        return overcraft;
+    }
+
+}
