4d0095a706b1a8ba012a1ec2dcbb55973db76631
mondgnu <mondgnu@gmail.com>
2023-06-28 17:30:58 +0200

    Made Overcraft final

--- src/main/java/de/yggdrasil/core/Overcraft.java
+++ src/main/java/de/yggdrasil/core/Overcraft.java
@@ -3,7 +3,7 @@ package de.yggdrasil.core;
 /**
  * This class provides a centralized point were you can put classes which are needed all over the project
  */
-public class Overcraft {
+public final class Overcraft {
     private static final Overcraft overcraft = new Overcraft();
 
     private Overcraft() {
